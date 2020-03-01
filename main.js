531297e58ac0ece7e2ed3fba29f8235c2c43267f
stepano9002 <suntrix3@gmail.com>
2020-03-01 16:08:56 +0900

    first commit2

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 require("dotenv").config();
+const config = require("./config.json");
 client.on("ready", () => {
   console.log(`i am ready!`);
 });
@@ -9,7 +10,7 @@ client.on("message", msg => {
   if (msg.author === client.user) {
     return;
   }
-  if (msg.content.indexOf(`${process.env.prefix}`) != -1) {
+  if (msg.content.indexOf(`${config.prefix}`) != -1) {
     msg.channel.send(msg.author.toString() + ":" + msg.content);
 
     imageNumber = Math.floor(Math.random() * 2) + 1;
