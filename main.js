f86d7533aa3ede4a30487a93cfc296c7a91b8638
stepano9002 <suntrix3@gmail.com>
2020-03-05 20:06:24 +0900

    필요없는 기능 삭제

--- main.js
+++ main.js
@@ -13,12 +13,6 @@ client.on("message", msg => {
 });
 
 client.on("message", msg => {
-  if (msg.content === "!세바스찬") {
-    msg.reply("최재혁(수드라/24세) : 왈-왈-");
-  }
-});
-
-client.on("message", msg => {
   if (msg.author === client.user) {
     return;
   }
